f60c0a79fc9cfb015db6444a9a0da800fbad76ce
samuelhatar <samuel.hatar@gmail.com>
2025-05-29 21:56:17 +0200

    First comimit

--- src/Main.java
+++ src/Main.java
@@ -232,7 +232,7 @@ public class Main {
         pole20[0][1][0] = -92;*/
 
 //smicka
-        for (int i = 1; i < 11; i++){
+        for (int i = 1; i < 101; i++){
             System.out.println(i +"Hello World");
         }
 
