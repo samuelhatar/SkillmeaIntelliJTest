3319ce6a392d35177e5801b032e2d3dfbb74924e
samuelhatar <samuel.hatar@gmail.com>
2025-05-29 18:22:41 +0200

    First comimit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -5,13 +7,82 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
 
-        String pozdrav = "Hello World";
+        /*String pozdrav = "Hello World";
 
         //Toto je pisanie textu
         System.out.println(pozdrav);
         System.out.println(pozdrav);
         System.out.println(pozdrav);
         System.out.println(pozdrav);
+
+        int prvniCislo = 10;
+        int druheCislo = 5;
+
+        prvniCislo = 20;
+
+        System.out.println(prvniCislo + druheCislo);
+        System.out.println(prvniCislo - druheCislo);
+        System.out.println(prvniCislo * druheCislo);
+        System.out.println(prvniCislo / druheCislo);
+
+        String pozdrav2 = "Ahoj";
+        String Meno = "Samuel";
+
+        System.out.println(pozdrav2 + Meno);
+
+        //Uloha Uzivatel napise svoje meno a program ho pozdravy
+
+        String pozdrav3 = "Ahoj, ";
+        Scanner mujScanner = new Scanner(System.in);
+        System.out.println("Zadaj svoje meno:");
+        String meno = mujScanner.nextLine();
+        System.out.println(pozdrav3 + meno);
+
+        //Kalkulacka
+        float prvecislo;
+        float druhecislo;
+        Scanner mujScanner2 = new Scanner(System.in);
+
+        System.out.println("Zadaj prve cislo:");
+        prvecislo = mujScanner2.nextFloat();
+
+        System.out.println("Zadaj druhe cislo:");
+        druhecislo = mujScanner2.nextFloat();
+
+        System.out.println(prvecislo + druhecislo);
+        System.out.println(prvecislo - druhecislo);
+        System.out.println(prvecislo * druhecislo);
+        System.out.println(prvecislo / druhecislo);
+
+       int nijakeCislo = 5;
+       nijakeCislo = 25;*/
+
+        /*Scanner mujScanner = new Scanner(System.in);
+        System.out.println("Zadaj svoje slovo:");
+        String slovo = mujScanner.nextLine();
+
+        System.out.println(slovo.length());
+         */
+
+        //Prevod typu
+        //int prevedeneCislo = Integer.parseInt("5"
+
+                /*int prveCislo = 25;
+                int druheCislo = 28;
+                //prveCislo = prveCislo + druheCislo;
+               //prveCislo += druheCislo;
+
+                System.out.println(prveCislo + druheCislo);
+                System.out.println(prveCislo - druheCislo);
+                System.out.println(prveCislo % druheCislo);
+
+                System.out.println(prveCislo <= druheCislo);*/
+
+
+        int cisloKPorovnanie = 9;
+        System.out.println(cisloKPorovnanie < 8 && cisloKPorovnanie <= 8);
+        System.out.println(cisloKPorovnanie > 8 || cisloKPorovnanie < 0);
+        System.out.println(!(cisloKPorovnanie > 8 || cisloKPorovnanie < 0));
 
 
 
